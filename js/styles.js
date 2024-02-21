f0a58ab738030d2258d39bf378d26f21a2e6c95d
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-02-21 18:56:09 +0600

    all done with footer

--- js/styles.js
+++ js/styles.js
@@ -1,3 +1,8 @@
 galleryImg.forEach(element => {
-  element.classList.add('min-w-full', 'h-64', 'border', 'bg-slate-500', 'rounded');
+  element.classList.add('min-w-full', 'h-64', 'border', 'border-black', 'bg-slate-500', 'rounded-2xl');
 });
+
+footerIcons.forEach(element => {
+  element.classList.add('h-12', 'rounded-full', 'bg-white', 'p-[1px]');
+});
+
--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,5 @@
 const galleryContainer = document.getElementById("gallery-container");
 const galleryImg = document.querySelectorAll("#gallery-container > *")
 const linkText = document.getElementById("link-input-box");
+const footerIcons = document.querySelectorAll("#footer-icons-container > *");
+
